1108a649ec7992dfddbe0d15d7a5649cb162f6fc
Fahchouch Mohammed <fahchouchsm@gmail.com>
2025-12-21 18:25:01 +0100

    Initial project setup for Spring Boot Gateway
    Add base files for a Spring Boot gateway application, including Maven wrapper, Dockerfile, project configuration, application source, test, and routing configuration. Sets up basic routing for jobfinder and eventhandler services and includes security and actuator dependencies.

---
+++ src/main/java/ehei/pfa/authGetway/AuthGetwayApplication.java
@@ -0,0 +1,12 @@
+package ehei.pfa.authGetway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuthGetwayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AuthGetwayApplication.class, args);
+	}
+}
---
+++ src/test/java/ehei/pfa/authGetway/AuthGetwayApplicationTests.java
@@ -0,0 +1,13 @@
+package ehei.pfa.authGetway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthGetwayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
