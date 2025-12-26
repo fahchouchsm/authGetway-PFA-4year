0edb5bb82cc90a0cd7a512f8345226c51e061156
Fahchouch Mohammed <fahchouchsm@gmail.com>
2025-12-26 17:32:57 +0100

    trying to switching to res256 decription

--- src/main/java/ehei/pfa/authGetway/security/JwtUtil.java
+++ src/main/java/ehei/pfa/authGetway/security/JwtUtil.java
@@ -6,10 +6,24 @@ import io.jsonwebtoken.SignatureAlgorithm;
 import io.jsonwebtoken.security.Keys;
 
 import java.security.Key;
+import java.security.KeyPair;
+import java.security.KeyPairGenerator;
 import java.util.Date;
 
 public class JwtUtil {
+    private static final KeyPair keyPair = generateKeyPair();
     private static final Key key = Keys.secretKeyFor(SignatureAlgorithm.HS256);
+
+    private static KeyPair generateKeyPair() {
+        try {
+            KeyPairGenerator generator = KeyPairGenerator.getInstance("RSA");
+            generator.initialize(2048);
+            return generator.generateKeyPair();
+        } catch (Exception ex) {
+            throw new RuntimeException("Failed to generate RS256 key pair", ex);
+        }
+    }
+
 
     public static String genToken(String userId) {
         return Jwts.builder().setSubject(userId).setExpiration(new Date(System.currentTimeMillis() + TIME.ONEDAY)).signWith(key).compact();
@@ -17,9 +31,5 @@ public class JwtUtil {
 
     public static String genToken(String userId, long exp) {
         return Jwts.builder().setSubject(userId).setExpiration(new Date(System.currentTimeMillis() + exp)).signWith(key).compact();
-    }
-
-    public static String validateToken(String token) {
-        return Jwts.parserBuilder().setSigningKey(key).build().parseClaimsJws(token).getBody().getSubject();
     }
 }
